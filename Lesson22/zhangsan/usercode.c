5d442f878f872ec5194a93b8b2462c68e26b615d
浪浪山小妖怪 <2116925408@qq.com>
2026-07-11 19:25:33 +0800

    动静态库

---
+++ Lesson22/zhangsan/usercode.c
@@ -0,0 +1,29 @@
+#include "mystdio.h"
+#include "mystring.h"
+#include <string.h>
+#include <unistd.h>
+
+int main()
+{
+    MyFile *filep = MyFopen("./log.txt", "a");
+    if(!filep)
+    {
+        printf("fopen error!\n");
+        return 1;
+    }
+
+    int cnt = 10;
+    while(cnt--)
+    {
+        char *msg = (char*)"hello myfile!!!";
+        MyFwrite(filep, msg, strlen(msg));
+        MyFFlush(filep);
+        printf("buffer: %s\n", filep->outbuffer);
+        sleep(1);
+    }
+    MyFclose(filep); // FILE *fp
+
+    const char *str = "hello bit!\n";
+    printf("strlen: %d\n",my_strlen(str));
+    return 0;
+}
