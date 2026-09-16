547ecaa623349a60c9b544722fe1a9c5fa05d313
isle-of-man-tt <2116925408@qq.com>
2026-09-16 23:30:18 +0800

    net

---
+++ Lesson43/Http/Http.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include "Socket.hpp"
+#include "TcpServer.hpp"
+#include <iostream>
+#include <string>
+#include <memory>
+#include <unordered_map>
+
+using namespace SocketModule;
+
+const std::string gspace = " ";
+const std::string glinespace = "\r\n";
+
+class HttpRequest
+{
+public:
+    HttpRequest()
+    {
+    }
+    std::string Serialize()
+    {
+        return std::string();
+    }
+    bool Deserialize(std::string &reqstr)
+    {
+        return true;
+    }
+    ~HttpRequest()
+    {
+    }
+
+private:
+    std::string _method;
+    std::string _url;
+    std::string _version;
+
+    std::unordered_map<std::string, std::string> _headers;
+    std::string _blankline;
+    std::string _text;
+};
+
+class Http
+{
+public:
+    Http(uint16_t port) : tsvrp(std::make_unique<TcpServer>(port))
+    {
+    }
+    void HandlerHttpRquest(std::shared_ptr<Socket> &sock, InetAddr &client)
+    {
+#ifndef DEBUG
+#define DEBUG
+        std::string httpreqstr;
+        sock->Recv(&httpreqstr); // 浏览器给我发过来的是一个大的http字符串
+        std::cout << httpreqstr;
+#endif
+        // 对请求字符串，进行反序列化
+    }
+    void Start()
+    {
+        tsvrp->Start([this](std::shared_ptr<Socket> &sock, InetAddr &client)
+                     { this->HandlerHttpRquest(sock, client); });
+    }
+    ~Http()
+    {
+    }
+
+private:
+    std::unique_ptr<TcpServer> tsvrp;
+};
